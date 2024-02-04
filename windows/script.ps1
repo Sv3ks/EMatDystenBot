6606dcb7f40991d11907f02047e82ca6f77d9a37
Sv3ks <89691080+Sv3ks@users.noreply.github.com>
2024-02-04 11:48:04 +0100

    Improvement + v1.1.0

---
+++ windows/script.ps1
@@ -0,0 +1,63 @@
+$VERSION = "v1.1.0"
+$URL = "https://emat.dk/"
+$SCRIPT1 = "ctl00_ContentPlaceHolder1_ImageButton_RegnehierarkietS.click()"
+$SCRIPT2 = "Link_Start2.click()"
+$SCRIPT3 = "for(let index=0;index<12;index++){Textbox_BrugersvarA.value=FacitA[ValgtSpg];img3.click();}"
+$SCRIPT4 = "img4.click()"
+$SCRIPT5 = "ctl00_ContentPlaceHolder1_ImageButton_VSelvStart.click()"
+Add-Type -AssemblyName System.Windows.Forms # Allows the script to use keyboard
+
+function StartNewExercise {
+	Set-Clipboard $SCRIPT1
+	[System.Windows.Forms.SendKeys]::SendWait("^(v){ENTER}")
+	Start-Sleep -Milliseconds 500
+	Set-Clipboard $SCRIPT2
+	[System.Windows.Forms.SendKeys]::SendWait("^(v){ENTER}")
+}
+
+function SolveExercise {
+	Set-Clipboard $SCRIPT3
+	[System.Windows.Forms.SendKeys]::SendWait("^(v){ENTER}")
+	Start-Sleep -Milliseconds 500
+	Set-Clipboard $SCRIPT4
+	[System.Windows.Forms.SendKeys]::SendWait("^(v){ENTER}")
+	Start-Sleep -Milliseconds 500
+	Set-Clipboard $SCRIPT5
+	[System.Windows.Forms.SendKeys]::SendWait("^(v){ENTER}")
+}
+
+function CheckForUpdates {
+	Write-Output "Checking for updates..."
+	$HTML = Invoke-WebRequest -Uri "https://sv3ks.github.io/EMatDystenBot/web/releases/latest/"
+	$LATEST = $HTML.ParsedHtml.body.document.getElementById('version').innerHTML
+	if ($VERSION.Equals($LATEST)) {
+		Write-Output "EMatDystenBot is up to date."
+	} else {
+		Write-Output "EMatDystenBot has a new update. (Press any key to start anyways)"
+		$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+	}
+}
+
+CheckForUpdates
+
+Start-Process chrome -ArgumentList "$URL" -PassThru | Out-Null # Open Website
+
+Write-Output "Please go through login process and return to harvesting site. (Press any key when done)"
+$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+Write-Output "Return to browser. Beginning in"
+Write-Output "3"
+Start-Sleep -Seconds 1
+Write-Output "2"
+Start-Sleep -Seconds 1
+Write-Output "1"
+Start-Sleep -Seconds 1
+Write-Output "Beginning exercise harvesting. You can stop at any moment by closing this window."
+[System.Windows.Forms.SendKeys]::SendWait("{F12}") # Open Console
+while($true) {
+	Start-Sleep -Seconds 1
+	StartNewExercise
+	Start-Sleep -Seconds 4 # Wait for exercise to begin
+	SolveExercise
+}
+Start-Sleep -Seconds 1
+$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
--- windows/start.bat
+++ windows/start.bat
@@ -1,4 +1,4 @@
 @echo off
-set "ScriptFile=bot.ps1"
+set "ScriptFile=script.ps1"
 set "ScriptPath=%~dp0%ScriptFile%"
 PowerShell.exe -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell.exe -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%ScriptPath%""' -Verb RunAs}"

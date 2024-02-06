3bc077676d30ec5bfaef25c39e2508a283db4e61
Sv3ks <snorre@silkjaer.dk>
2024-02-06 10:12:37 +0100

    woopsie + fix :)

--- go/bot.go
+++ go/bot.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"os"
 	"os/exec"
 	"runtime"
 	"time"
@@ -10,13 +13,42 @@ import (
 	"github.com/gocolly/colly"
 )
 
+type Config struct {
+	Version string `json:"version"`
+	Delay   int    `json:"delay"`
+}
+
 func main() {
-	version := "v1.2.2"
-	fmt.Println("Starting EMatDystenBot " + version)
+	fmt.Println("Loading files...")
+
+	// Create default config
+	var defaultConfig Config
+	defaultConfig.Delay = 250
+	defaultConfig.Version = "v1.2.2"
+
+	var config Config
+	configPath := "config.json"
+
+	if FileExists(configPath) {
+		config = LoadConfig(configPath)
+		fmt.Println(config)
+		fmt.Println("Loaded config.json")
+	} else {
+		// Create config from default config
+		config = CreateConfig(defaultConfig, configPath)
+		fmt.Println("Created config.json")
+	}
+
+	fmt.Println("Loaded files")
+
+	fixedDelay := time.Duration(config.Delay) * time.Millisecond
+
+	fmt.Println("Starting EMatDystenBot " + config.Version)
+
 	// Checking for updates
 	fmt.Println("Checking for updates...")
 	latest := getLatestVersion()
-	if version == latest {
+	if config.Version == latest {
 		fmt.Println("You're running the latest version of EMatDystenBot.")
 		fmt.Println("(Press enter to start)")
 	} else {
@@ -48,32 +80,103 @@ func main() {
 	time.Sleep(1 * time.Second)
 	fmt.Println("Starting in 1")
 	time.Sleep(1 * time.Second)
-	fmt.Println("Starting to harvest. You can stop at any time by")
+	fmt.Println("Starting to harvest. You can stop at any time by closing this window.")
 
-	GoToStartingPage()
+	GoToStartingPage(fixedDelay)
 
-	time.Sleep(250 * time.Millisecond)
+	time.Sleep(fixedDelay)
 
-	// Write text in console
-
-	fmt.Scanln()
+	for true {
+		StartExercise(fixedDelay)
+		time.Sleep(fixedDelay)
+		SolveExercise(fixedDelay)
+		time.Sleep(fixedDelay)
+	}
 }
 
-func StartExercise() {
+func FileExists(filePath string) bool {
+	_, err := os.Stat(filePath)
 
+	if os.IsNotExist(err) {
+		return false
+	} else {
+		return true
+	}
 }
 
-func SolveExercise() {
+func CreateConfig(config Config, path string) Config {
+	data, err := json.MarshalIndent(config, "", "  ")
+	if err != nil {
+		fmt.Println("Error marshalling JSON:", err)
+		return config
+	}
 
+	file, err := os.Create(path)
+	if err != nil {
+		fmt.Println("Error creating file:", err)
+		return config
+	}
+	defer file.Close()
+
+	_, err = file.Write(data)
+	if err != nil {
+		fmt.Println("Error writing to file:", err)
+		return config
+	}
+
+	return config
 }
 
-func GoToStartingPage() {
+func LoadConfig(path string) Config {
+	var config Config
+
+	file, err := os.Open(path)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return config
+	}
+	defer file.Close()
+
+	data, err := ioutil.ReadAll(file)
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		return config
+	}
+
+	err = json.Unmarshal(data, &config)
+	if err != nil {
+		fmt.Println("Error unmarshalling JSON:", err)
+	}
+
+	return config
+}
+
+func StartExercise(delay time.Duration) {
+	robotgo.TypeStr("ctl00_ContentPlaceHolder1_ImageButton_RegnehierarkietS.click()")
+	robotgo.KeyTap("enter")
+	time.Sleep(delay)
+	robotgo.TypeStr("Link_Start2.click()")
+	robotgo.KeyTap("enter")
+}
+
+func SolveExercise(delay time.Duration) {
+	robotgo.TypeStr("for(let index=0;index<13;index++){Textbox_BrugersvarA.value=FacitA[ValgtSpg];img3.click();}")
+	robotgo.KeyTap("enter")
+	time.Sleep(delay)
+	robotgo.TypeStr("img4.click()")
+	robotgo.KeyTap("enter")
+	time.Sleep(delay)
+	robotgo.TypeStr("ctl00_ContentPlaceHolder1_ImageButton_VSelvStart.click()")
+	robotgo.KeyTap("enter")
+}
+
+func GoToStartingPage(delay time.Duration) {
 	robotgo.TypeStr("ctl00_ImageButton_Dysten.click()")
 	robotgo.KeyTap("enter")
-	time.Sleep(250 * time.Millisecond)
+	time.Sleep(delay)
 	robotgo.TypeStr("ctl00_ContentPlaceHolder1_ImageButton_StartDysten.click()")
 	robotgo.KeyTap("enter")
-	time.Sleep(250 * time.Millisecond)
+	time.Sleep(delay)
 	robotgo.TypeStr("ctl00_ContentPlaceHolder1_ImageButton_StartNormal.click()")
 	robotgo.KeyTap("enter")
 }
